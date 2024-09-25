88ba0e63a8f340f06366f3d3442d9246c93b2432
lancefinke <lance_finke@comcast.net>
2024-09-24 22:02:36 -0500

    feat: seperated form compenent into seperate file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
+import { NewTodoForm } from "./form"
 import "./styles.css"
 import { useState } from "react"
 
 export default function App(){
-  const [newitem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function addTodo(title){
+    setTodos((currentTodos) => {
+      return [...currentTodos,
+        {
+          id: crypto.randomUUID(), title, completed: false
+        },
+      ]
+    })
+  }
+
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id){
+          return {...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
 
   return (
     <>
-    <form className="new-item-form">
-      <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
-      </div>
-      <button className="btn add-btn">ADD</button>
-    </form>
-    <h1 className="header">To-Do List</h1>
-    <ol className="list">
-      
-    </ol>
+    <NewTodoForm onSubmit={addTodo}/>
+    <div className="list-box">
+      <h1 className="header">To-Do List</h1>
+      <ol className="list">
+        {todos.map(todo => {
+          return <li key={todo.id}> 
+            <label>
+              <input type="checkbox" checked={todo.checked}
+              onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+              {todo.title}
+            </label>
+            <button className="btn btn-delete" onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        })}
+      </ol>
+    </div>
     </>
   )
 }
