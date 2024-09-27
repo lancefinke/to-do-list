b97ec957323fbba3cb1312f125fd45bbdd754503
lancefinke <lance_finke@comcast.net>
2024-09-26 20:53:26 -0500

    fix: centered the input and add components

--- src/form.js
+++ src/form.js
@@ -21,14 +21,12 @@ export function NewTodoForm({ onSubmit }){
     return(
         <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
-          <label htmlFor="item">
           <input 
           value={newItem} 
           onChange={e => setNewItem(e.target.value)} 
           type="text" 
           id="item" 
           placeholder="Enter in a new task..."/>
-          </label>
       </div>
       <button className="btn add-btn">ADD</button>
     </form>
